e0aaa16d3b57de9cf15c99b6eed7bf2383250ea9
cuigh <noname@live.com>
2021-09-02 11:54:23 +0800

    Regex rule skips empty string

--- data/valid/rule.go
+++ data/valid/rule.go
@@ -14,6 +14,22 @@ import (
 	"github.com/cuigh/auxo/ext/reflects"
 )
 
+const (
+	PatternEmail        = `^(?i)([A-Za-z0-9!#$%&'*+\/=?^_{|.}~-]+@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)$`
+	PatternURL          = `^(?:(?:https?:\/\/)?(?:[a-z0-9.\-]+|www|[a-z0-9.\-])[.](?:[^\s()<>]+|\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))*\))+(?:\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))*\)|[^\s!()\[\]{};:\'".,<>?]))$`
+	PatternAlpha string = `^[a-zA-Z]+$`
+	//PatternAlphaNumeric string = `^[a-zA-Z0-9]+$`
+	//PatternIPV4  = `^(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`
+	//PatternIPV6  = `^(?:(?:(?:[0-9A-Fa-f]{1,4}:){7}(?:[0-9A-Fa-f]{1,4}|:))|(?:(?:[0-9A-Fa-f]{1,4}:){6}(?::[0-9A-Fa-f]{1,4}|(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(?:(?:[0-9A-Fa-f]{1,4}:){5}(?:(?:(?::[0-9A-Fa-f]{1,4}){1,2})|:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(?:(?:[0-9A-Fa-f]{1,4}:){4}(?:(?:(?::[0-9A-Fa-f]{1,4}){1,3})|(?:(?::[0-9A-Fa-f]{1,4})?:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(?:(?:[0-9A-Fa-f]{1,4}:){3}(?:(?:(?::[0-9A-Fa-f]{1,4}){1,4})|(?:(?::[0-9A-Fa-f]{1,4}){0,2}:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(?:(?:[0-9A-Fa-f]{1,4}:){2}(?:(?:(?::[0-9A-Fa-f]{1,4}){1,5})|(?:(?::[0-9A-Fa-f]{1,4}){0,3}:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(?:(?:[0-9A-Fa-f]{1,4}:){1}(?:(?:(?::[0-9A-Fa-f]{1,4}){1,6})|(?:(?::[0-9A-Fa-f]{1,4}){0,4}:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(?::(?:(?:(?::[0-9A-Fa-f]{1,4}){1,7})|(?:(?::[0-9A-Fa-f]{1,4}){0,5}:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(?:%.+)?\s*$`
+	//PatternIP    = PatternIPV4 + `|` + PatternIPV6
+)
+
+var (
+	regexEmail = regexp.MustCompile(PatternEmail)
+	regexURL   = regexp.MustCompile(PatternURL)
+	regexAlpha = regexp.MustCompile(PatternAlpha)
+)
+
 var (
 	rules = map[string]Rule{
 		//"width":    nil, // string width, for CJK character, width is 2
@@ -99,8 +115,10 @@ func matchRule(name string, r *regexp.Regexp) Rule {
 			return err
 		}
 
-		s := v.String()
-		return assert(r.MatchString(s), name, ctx, arg, s)
+		if s := v.String(); s != "" {
+			return assert(r.MatchString(s), name, ctx, arg, s)
+		}
+		return nil
 	}
 }
 
@@ -147,9 +165,11 @@ func regexRule(ctx *Context, arg *Argument) error {
 		return err
 	}
 
-	s := v.String()
-	ok, _ := regexp.MatchString(arg.Value, s)
-	return assert(ok, "regex", ctx, arg, s)
+	if s := v.String(); s != "" {
+		ok, _ := regexp.MatchString(arg.Value, s)
+		return assert(ok, "regex", ctx, arg, s)
+	}
+	return nil
 }
 
 // lengthRule can apply to string/slice/map/chan type.
